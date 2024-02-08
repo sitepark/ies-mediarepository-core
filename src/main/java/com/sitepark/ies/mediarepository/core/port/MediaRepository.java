3de20be66c9052725a828c8bc0e336c36623b8d7
Holger Veltrup <92872893+sitepark-veltrup@users.noreply.github.com>
2024-02-08 09:45:12 +0100

    style: format with google java style guide

--- src/main/java/com/sitepark/ies/mediarepository/core/port/MediaRepository.java
+++ src/main/java/com/sitepark/ies/mediarepository/core/port/MediaRepository.java
@@ -1,17 +1,20 @@
 package com.sitepark.ies.mediarepository.core.port;
 
-import java.util.List;
-
 import com.sitepark.ies.mediarepository.core.domain.entity.MediaReference;
+import java.util.List;
 
 /**
  * Interface of the media repository that defines all the methods
  * required for the use cases.
  */
 public interface MediaRepository {
-	void addReference(MediaReference reference);
-	void remove(String mediaId);
-	void removeReferencesUsedBy(String usedBy);
-	List<MediaReference> getReferencesUsedBy(String usedBy);
-	List<MediaReference> getReferencesByMedia(String mediaId);
+  void addReference(MediaReference reference);
+
+  void remove(String mediaId);
+
+  void removeReferencesUsedBy(String usedBy);
+
+  List<MediaReference> getReferencesUsedBy(String usedBy);
+
+  List<MediaReference> getReferencesByMedia(String mediaId);
 }
--- src/main/java/com/sitepark/ies/mediarepository/core/usecase/RemoveByReference.java
+++ src/main/java/com/sitepark/ies/mediarepository/core/usecase/RemoveByReference.java
@@ -1,13 +1,11 @@
 package com.sitepark.ies.mediarepository.core.usecase;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import com.sitepark.ies.mediarepository.core.domain.entity.MediaReference;
 import com.sitepark.ies.mediarepository.core.domain.entity.MediaReferenceType;
 import com.sitepark.ies.mediarepository.core.port.MediaRepository;
-
 import jakarta.inject.Inject;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Removes a medium via its reference. If this is the only or last reference
@@ -16,58 +14,60 @@ import jakarta.inject.Inject;
  */
 public final class RemoveByReference {
 
-	private final MediaRepository repository;
+  private final MediaRepository repository;
 
-	@Inject
-	protected RemoveByReference(MediaRepository repository) {
-		this.repository = repository;
-	}
+  @Inject
+  protected RemoveByReference(MediaRepository repository) {
+    this.repository = repository;
+  }
 
-	public void removeByReference(String usedBy) {
+  public void removeByReference(String usedBy) {
 
-		List<MediaReference> embeddedReferenceList =
-				this.repository.getReferencesUsedBy(usedBy).stream()
-				.filter(ref -> ref.getType() == MediaReferenceType.EMBEDDED)
-				.collect(Collectors.toList());
+    List<MediaReference> embeddedReferenceList =
+        this.repository.getReferencesUsedBy(usedBy).stream()
+            .filter(ref -> ref.getType() == MediaReferenceType.EMBEDDED)
+            .collect(Collectors.toList());
 
-		this.repository.removeReferencesUsedBy(usedBy);
+    this.repository.removeReferencesUsedBy(usedBy);
 
-		List<String> mediaList = embeddedReferenceList.stream()
-				.map(ref -> ref.getMediaId())
-				.distinct()
-				.collect(Collectors.toList());
+    List<String> mediaList =
+        embeddedReferenceList.stream()
+            .map(ref -> ref.getMediaId())
+            .distinct()
+            .collect(Collectors.toList());
 
-		this.removeMediaOnlyAssociatedWithDeletedEmbeddedReference(usedBy, mediaList);
-	}
+    this.removeMediaOnlyAssociatedWithDeletedEmbeddedReference(usedBy, mediaList);
+  }
 
-	@SuppressWarnings("PMD.AvoidLiteralsInIfCondition")
-	private void removeMediaOnlyAssociatedWithDeletedEmbeddedReference(String usedBy, List<String> mediaList) {
-		for (String mediaId : mediaList) {
-			List<MediaReference> mediaReferenceList = this.repository.getReferencesByMedia(mediaId);
+  @SuppressWarnings("PMD.AvoidLiteralsInIfCondition")
+  private void removeMediaOnlyAssociatedWithDeletedEmbeddedReference(
+      String usedBy, List<String> mediaList) {
+    for (String mediaId : mediaList) {
+      List<MediaReference> mediaReferenceList = this.repository.getReferencesByMedia(mediaId);
 
-			/*
-			 * The media repository does not have its own referrer table yet and this.repository.getReferencesByMedia()
-			 * therefore still uses the LinkContent table.
-			 *
-			 * this.repository.removeReferencesUsedBy(usedBy); also has no effect yet. Therefore
-			 * mediaReferenceList still contains the usedBy id. Therefore it is first queried,
-			 * whether the list size = 1 and contains usedBy.
-			 *
-			 * With a real reference table for the mediaRepository then applies:
-			 *
-			if (mediaReferenceList.isEmpty()) {
-				this.repository.remove(mediaId);
-			}
-			*/
+      /*
+       * The media repository does not have its own referrer table yet and this.repository.getReferencesByMedia()
+       * therefore still uses the LinkContent table.
+       *
+       * this.repository.removeReferencesUsedBy(usedBy); also has no effect yet. Therefore
+       * mediaReferenceList still contains the usedBy id. Therefore it is first queried,
+       * whether the list size = 1 and contains usedBy.
+       *
+       * With a real reference table for the mediaRepository then applies:
+       *
+      if (mediaReferenceList.isEmpty()) {
+      	this.repository.remove(mediaId);
+      }
+      */
 
-			if (mediaReferenceList.isEmpty()) {
-				this.repository.remove(mediaId);
-			} else if (mediaReferenceList.size() == 1) {
-				MediaReference mediaReference = mediaReferenceList.get(0);
-				if (mediaReference.getUsedBy().equals(usedBy)) {
-					this.repository.remove(mediaId);
-				}
-			}
-		}
-	}
+      if (mediaReferenceList.isEmpty()) {
+        this.repository.remove(mediaId);
+      } else if (mediaReferenceList.size() == 1) {
+        MediaReference mediaReference = mediaReferenceList.get(0);
+        if (mediaReference.getUsedBy().equals(usedBy)) {
+          this.repository.remove(mediaId);
+        }
+      }
+    }
+  }
 }
